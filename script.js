05518e72c6278104cfdf53802c44348c273e78d3
FIXED-TERM Joshi Pushpesh (MS/ECJ2-PS) <huo3kor@bosch.com>
2024-04-01 16:50:31 +0530

    completed Logic and UI

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 'use strict'
 
 const btns=document.querySelectorAll('.btn');
-let playerScore=0, computerScore=0, rounds=1;
+let playerScore=0, computerScore=0, rounds=0;
 const choices=['rock','paper', 'scissors'];
 
 const displayPlayerScore=document.querySelector('.playerScores .score');
 const displayComputerScore=document.querySelector('.computerScores .score');
 const roundNumber=document.querySelector('.roundNo');
 const roundWinner=document.querySelector('.roundWinner');
+const overlay=document.querySelector('.overlay');
+const finalWinnerMessage=document.querySelector('.overlay .message');
+const retryBtn=document.querySelector('.retry');
 
 
 btns.forEach(function(btn){
@@ -16,22 +19,29 @@ btns.forEach(function(btn){
         console.log(playerChoice);
         const computerChoice=getComputerChoice();
         let winner=getWinner(playerChoice,computerChoice);
-
         if (winner==='player'){
             playerScore++;
+            roundWinner.textContent=`You win! ${playerChoice} beats ${computerChoice}`;         
         }
-
+        
         if(winner==='computer'){
             computerScore++;
+            roundWinner.textContent=`You lose! ${computerChoice} beats ${playerChoice}`;
+        }
+
+        if(winner=='tie')
+        {
+            roundWinner.textContent='It\'s a Tie!'
         }
 
         displayPlayerScore.textContent=playerScore;
         displayComputerScore.textContent=computerScore;
 
-        ++rounds;
-
+        rounds++;
+        roundNumber.textContent=rounds;
+        console.log(rounds);
         if (rounds>4)
-        endgame();
+        endGame();
     })
 })
 
@@ -41,9 +51,7 @@ function getComputerChoice(){
 }
 
 function getWinner(playerChoice,computerChoice){
-    let message='';
     if (playerChoice === computerChoice) {
-        message = "It's a tie";
         return "tie";
       } 
 
@@ -52,16 +60,37 @@ function getWinner(playerChoice,computerChoice){
         (playerChoice === "paper" && computerChoice === "rock") ||
         (playerChoice === "scissors" && computerChoice === "paper")
     ) {
-        message = `You win! ${playerChoice} beats ${computerChoice}`;
         return "player";
     }
 
     else {
-        message = `You lose! ${computerChoice} beats ${playerChoice}`;
         return "computer";
     }
 }
 
 function endGame(){
-    
+    let winner='';
+    if (playerScore > computerScore) {
+        winner="You win";
+      } 
+      else if (playerScore === computerScore) {
+        winner="It's a draw!";
+      } 
+      else {
+        winner="Computer wins!";
+      }
+      finalWinnerMessage.textContent=winner;
+      overlay.style.display='block';      
+      retryBtn.addEventListener('click',restartGame);
+}
+
+function restartGame(){
+    overlay.style.display='none';
+    playerScore=0;
+    computerScore=0;
+    rounds=0;
+    displayPlayerScore.textContent=playerScore;
+    displayComputerScore.textContent=computerScore;
+    roundNumber.textContent=rounds;
+
 }
